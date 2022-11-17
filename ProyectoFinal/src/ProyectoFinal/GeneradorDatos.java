b4dc68658448aad3e0182dee894917b4e3423e53
Ian Alexis Solis Hernández <90357408+SolisAlexis24@users.noreply.github.com>
2022-11-16 21:49:00 -0600

    Modulo correcto

--- ProyectoFinal/src/ProyectoFinal/GeneradorDatos.java
+++ ProyectoFinal/src/ProyectoFinal/GeneradorDatos.java
@@ -2,6 +2,7 @@ package ProyectoFinal;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,13 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class GeneradorNombres {
+public class GeneradorDatos {
     public String nom = "";
     public String ape = "";
     public String nombresGen = "";
     List<String> nomList = new ArrayList<>();
     List<String> apeList = new ArrayList<>();
+    List<String> nomGenList = new ArrayList<>();
+    List<Integer> EdadesList = new ArrayList<>();
 
     public void leer(){
         int cont = 0;
@@ -63,6 +68,7 @@ public class GeneradorNombres {
                     int r2 = (int)(Math.random()*50);
                     int r3 = (int)(Math.random()*200);
                     nombresGen += nomList.get(r1) + " "+ nomList.get(r2)+" "+ apeList.get(r3)+ " " + apeList.get(r1) + ",";
+                    nomGenList.add(nomList.get(r1) + " "+ nomList.get(r2)+" "+ apeList.get(r3)+ " " + apeList.get(r1));
                 }
                 //Para nombres de hombre (51, 100)
                 else{
@@ -70,9 +76,9 @@ public class GeneradorNombres {
                     int r2 = (int)(Math.random()*(100-51)+50);  
                     int r3 = (int)(Math.random()*200);
                     nombresGen += nomList.get(r1) + " "+ nomList.get(r2)+" "+ apeList.get(r3)+ " " + apeList.get(r1) + ",";
+                    nomGenList.add(nomList.get(r1) + " "+ nomList.get(r2)+" "+ apeList.get(r3)+ " " + apeList.get(r1));
                 }
             }
-            System.out.println("Nombres generados");
             FileWriter fw = new FileWriter("NombresGenerados.csv");
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter sd = new PrintWriter(bw);
@@ -82,6 +88,24 @@ public class GeneradorNombres {
             System.out.println("No fue posible abrir el archivo");
         }
     }
-
+    public void generadorEdad(){
+        for (int i = 0; i <= 500; i++) {
+            int r1 = (int)(Math.random()*(25-19)+18);
+            EdadesList.add(r1);
+        }
+    }
+    public void generadorNumCuenta(){
+        String numCuenta = "";
+        String numsCuenta = "";
+        for (int i = 0; i <= 500; i++) {
+            for (int j = 0; j < 9; j++) {
+                int r1 = (int)(Math.random()*(9-1)+0);
+                numCuenta += String.valueOf(r1);
+            }
+            numsCuenta += numCuenta + ",";
+            numCuenta = "";
+        }
+        System.out.println(numsCuenta);
+    }
 }
 
