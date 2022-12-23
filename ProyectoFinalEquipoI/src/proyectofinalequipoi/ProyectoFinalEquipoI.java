cc71e51e8338dd261400172a77b1b2d26e71101a
Ian Alexis Solis Hernández <90357408+SolisAlexis24@users.noreply.github.com>
2022-12-23 14:52:18 -0600

    Update ProyectoFinalEquipoI.java

--- ProyectoFinalEquipoI/src/proyectofinalequipoi/ProyectoFinalEquipoI.java
+++ ProyectoFinalEquipoI/src/proyectofinalequipoi/ProyectoFinalEquipoI.java
@@ -1,16 +1,15 @@
 package proyectofinalequipoi;
 
-/**
- *
- * @author Alexis
- */
+import Vista.login;
+
 public class ProyectoFinalEquipoI {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        login menu = new Vista.login();
+        menu.loginin();
     }
     
 }
